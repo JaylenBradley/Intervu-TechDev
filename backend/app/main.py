9640305c67993060123b6a7bfb9ecce34bbb7178
JayBradCoding <jbradl24@villanova.edu>
2025-07-22 17:00:28 -0400

    Added DS_Store to gitignore and fixed cors middleware in backend for vercel connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://your-vercel-frontend.vercel.app",
+        "https://useintervu.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
